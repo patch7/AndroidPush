ee5c4c13579dd2ea1e7c170f4f351bb63723da66
patch <patch_m@mail.ru>
2018-10-23 21:09:15 +0300

    Добавил код для скачивания страницы

--- app/src/main/java/com/example/patch/myapplication/MainActivity.java
+++ app/src/main/java/com/example/patch/myapplication/MainActivity.java
@@ -1,38 +1,90 @@
 package com.example.patch.myapplication;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
+    private EditText eText;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.text_view);
+        eText = (EditText) findViewById(R.id.URL);
+        btn = (Button) findViewById(R.id.connect_button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Connecting().execute();
+            }
+        });
     }
 
-    public void set()
+    class Connecting extends AsyncTask<String, Void, String>
     {
-        ;
-    }
+        @Override
+        protected String doInBackground(String... params)
+        {
+            HttpURLConnection urlConnection = null;
+            try
+            {
+                URL Url = new URL(eText.getText().toString());
+                urlConnection = (HttpURLConnection) Url.openConnection();
+                urlConnection.setReadTimeout(10000/*milliseconds*/);
+                urlConnection.setConnectTimeout(15000/*milliseconds*/);
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setDoInput(true);
+                urlConnection.connect();
+                int response = urlConnection.getResponseCode();
+                StringBuilder buf = new StringBuilder();
 
-    public void get()
-    {
-        ;
-    }
+                if(response == HttpURLConnection.HTTP_OK);
+                {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    String line = null;
+                    while ((line = reader.readLine()) != null)
+                        buf.append(line);
+                }
+                return buf.toString();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                if(urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return null;
+        }
 
-    public void name()
-    {
-        ;
-    }
+        @Override
+        protected void onPostExecute(String result)
+        {
+            text.setText(result);
+        }
 
-    public void setName()
-    {
-        ;
-    }
-
-    public void getName()
-    {
-        ;
+        @Override
+        protected void onPreExecute()
+        {
+            ;
+        }
     }
 }
