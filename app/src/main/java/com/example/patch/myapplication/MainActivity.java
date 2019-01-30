ff710adbf4c45f9ece15bdc0ddec0aa6caaec07c
patch <patch_m@mail.ru>
2019-01-30 11:46:19 +0300

    Добавлено сохранение в файл (только перезапись файла).

--- app/src/main/java/com/example/patch/myapplication/MainActivity.java
+++ app/src/main/java/com/example/patch/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.patch.myapplication;
 
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -12,14 +13,24 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
   private TextView text;
   private Button   btn;
+  private Button   clear;
+  private Button   save;
 
-  class Connecting extends AsyncTask<String, Void, String> {
+  class Connecting extends AsyncTask<String, Void, String>
+  {
     @Override protected void onPreExecute() {}
 
-    @Override protected String doInBackground(String... params) {
+    @Override protected String doInBackground(String... params)
+    {
       try {
         StringBuilder buf = new StringBuilder();
 
@@ -47,27 +58,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
       return null;
     }
 
-    @Override protected void onPostExecute(String result) {
+    @Override protected void onPostExecute(String result)
+    {
       text.setText(result);
       text.setMovementMethod(new ScrollingMovementMethod());
     }
   }
 
-  @Override public void onClick(View v) {
+  @Override public void onClick(View v)
+  {
     switch (v.getId()) {
-      case R.id.connect_button:
-        new Connecting().execute();
-        break;
+      case R.id.connect_button: new Connecting().execute(); break;
+      case R.id.clear_button:   text.setText("");           break;
+      case R.id.save_btn:       saveInFileSD();             break;
     }
   }
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
+  @Override protected void onCreate(Bundle savedInstanceState)
+  {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
     text  = (TextView) findViewById(R.id.text_view);
     btn   = (Button)   findViewById(R.id.connect_button);
+    clear = (Button)   findViewById(R.id.clear_button);
+    save  = (Button)   findViewById(R.id.save_btn);
 
     btn.setOnClickListener(this);
+    clear.setOnClickListener(this);
+    save.setOnClickListener(this);
+  }
+
+  private void saveInFileSD()
+  {
+    if(!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED))
+      text.setText("Save failed!\nSD card not found!");
+
+    File path = Environment.getExternalStorageDirectory();
+    File file = new File(path, "DB.txt");
+
+    try{
+      BufferedWriter bwrite = new BufferedWriter(new FileWriter(file));
+      bwrite.write(text.getText().toString());
+      bwrite.close();
+      text.setText("Successful!");
+    }
+    catch(IOException e) {
+      text.setText(e.getMessage() + "Exception");
+    }
   }
 }
